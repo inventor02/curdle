994fcfda03e1f73786130f9908e8bcd536d1c361
George Peppard <gjp1g21@soton.ac.uk>
2022-01-22 23:45:19 +0000

    sdl works!

--- main.c
+++ main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <SDL.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    printf("error init sdl: %s\n", SDL_GetError());
+    return EXIT_FAILURE;
+  }
+
+  SDL_Window *window = SDL_CreateWindow(
+      "curdle",
+      SDL_WINDOWPOS_CENTERED,
+      SDL_WINDOWPOS_CENTERED,
+      640,
+      640,
+      0
+  );
+
+  if (window == NULL) {
+    printf("error init sdl window: %s\n", SDL_GetError());
+    return EXIT_FAILURE;
+  }
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+
+  if (renderer == NULL) {
+    printf("error init sdl renderer: %s\n", SDL_GetError());
+    return EXIT_FAILURE;
+  }
+
+  SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
+  SDL_RenderClear(renderer);
+  SDL_RenderPresent(renderer);
+
+  SDL_Delay(3000);
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
 }
