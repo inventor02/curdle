be49294c7219dc02084791c0552c55fd17aa149f
Toby Godfrey <toby.godfrey2003@gmail.com>
2022-01-29 21:56:36 +0000

    break!

--- game.c
+++ game.c
@@ -90,6 +90,7 @@ void append_letter(struct game *game, char current_letter){
   for(uint8_t i = 0; i < CURDLE_WORD_LENGTH; i++){
     if(game->current_guess[i] == NULL){
       game->current_guess[i] = current_letter;
+      break;
     }
   }
 }
