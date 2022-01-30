0e13f43ab42643a5dc99f168272c1ba98dad08cc
Adam <adam.caine10@gmail.com>
2022-01-30 16:01:56 +0000

    Created draw_tile method

--- render.c
+++ render.c
@@ -1,5 +1,4 @@
 #include "render.h"
-#include <SDL2/SDL_ttf.h>
 #include <unistd.h>
 #include <limits.h>
 #include "game.h"
@@ -20,7 +19,7 @@ int start_window() {
   TTF_Init();
 
   // Create a window centered on the screen with set width and height
-  window = SDL_CreateWindow("Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 240 * CURDLE_WINDOW_SCALE, 135 * CURDLE_WINDOW_SCALE, 0);
+  window = SDL_CreateWindow("Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 150 * CURDLE_WINDOW_SCALE, 240 * CURDLE_WINDOW_SCALE, 0);
 
   if (window == NULL) {
     printf("Error creating window: %s\n", SDL_GetError());
@@ -36,11 +35,6 @@ int start_window() {
     return CURDLE_SDL_RENDERER_FAILURE;
   }
 
-  // Set background colour
-  SDL_SetRenderDrawColor(renderer, 96, 96, 96, 255);
-
-  // Clear window
-  SDL_RenderClear(renderer);
 
   // Get the currentPath
 
@@ -56,7 +50,7 @@ int start_window() {
   // Render Text??
 
   // Load font style and set size
-  TTF_Font* curdle_font = TTF_OpenFont("../res/Roboto-Black.ttf", 24);
+  TTF_Font* curdle_font = TTF_OpenFont("../res/Roboto-Black.ttf", 256);
 
   // Load custom text colour
   SDL_Color curdle_font_colour = {CURDLE_TEXT_COLOUR_R, CURDLE_TEXT_COLOUR_G, CURDLE_TEXT_COLOUR_B, 255};
@@ -84,12 +78,15 @@ int start_window() {
 
 
 
-  event_poll(window, renderer);
+  event_poll(window, renderer, curdle_font);
 
 
   // Close and destroy the window and renderer
   SDL_DestroyWindow(window);
   SDL_DestroyRenderer(renderer);
+
+  // Close the font
+  TTF_CloseFont(curdle_font);
 
   TTF_Quit();
 
@@ -99,7 +96,7 @@ int start_window() {
   return EXIT_SUCCESS;
 }
 
-int event_poll(SDL_Window* window, SDL_Renderer* renderer) {
+int event_poll(SDL_Window* window, SDL_Renderer* renderer, TTF_Font* font) {
 
   // Create our event
   SDL_Event event;
@@ -137,6 +134,31 @@ int event_poll(SDL_Window* window, SDL_Renderer* renderer) {
 
 
     // Rendering goes here
+
+    // Set background colour
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+    // Clear window
+    SDL_RenderClear(renderer);
+
+    SDL_Rect tile;
+    // Render test tile?
+
+    char test_char_y[2] = {'Y','\0'};
+    char test_char_o[2] = {'O', '\0'};
+    draw_tile(&tile, renderer, RIGHT_WRONG_POSITION, 0, 0, test_char_y, font);
+    draw_tile(&tile, renderer, RIGHT_RIGHT_POSITION, 0, 1, test_char_o, font);
+    draw_tile(&tile, renderer, WRONG, 0, 2, test_char_y, font);
+    draw_tile(&tile, renderer, WRONG, 0, 3, test_char_o, font);
+    draw_tile(&tile, renderer, RIGHT_RIGHT_POSITION, 0, 4, test_char_y, font);
+
+    for (uint8_t row = 1; row < 6; row++) {
+      for (uint8_t column = 0; column < 5; column++) {
+        draw_tile(&tile, renderer, BLANK, row, column, NULL, NULL);
+      }
+    }
+    //draw_tile(tile, renderer, RIGHT_RIGHT_POSITION, 0, 0, test_char, font);
+    SDL_RenderPresent(renderer);
   }
 
 
@@ -160,5 +182,70 @@ void draw_rect(SDL_Renderer* renderer) {
   SDL_RenderPresent(renderer);
 
   // Render the rectangle to the screen
+
+}
+
+void draw_tile(SDL_Rect* tile, SDL_Renderer* renderer, enum rectangle_draw_type type, uint8_t row, uint8_t column, char* letter, TTF_Font* font) {
+
+  // Positioning as follows:
+  // 240px of height for aspect ratio of one
+  // 6 rows - Make 9 rows with one row of pixels shared between the rows
+  // 240px / 10 = 24
+  // Rows numbered from 0-5
+  
+  // Columns
+  // 135px / 9 = 15
+  // 2 columns either side, with
+
+  tile->x = (7 + (column * 28)) * CURDLE_WINDOW_SCALE;
+  tile->y = (48 + (row * 28)) * CURDLE_WINDOW_SCALE;
+  tile->w = 24 * CURDLE_WINDOW_SCALE;
+  tile->h = 24 * CURDLE_WINDOW_SCALE;
+
+  switch (type)
+  {
+  case BLANK:
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    break;
+  case WRONG:
+    SDL_SetRenderDrawColor(renderer, 128, 128, 128, 255);
+    break;
+  case RIGHT_WRONG_POSITION:
+    SDL_SetRenderDrawColor(renderer, 25, 178, 255, 255);
+    break;
+  case RIGHT_RIGHT_POSITION:
+    SDL_SetRenderDrawColor(renderer, 255, 102, 128, 255);
+    break;
+  
+  default: 
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    break;
+  }
+
+  SDL_RenderFillRect(renderer, tile);
+  // Render the outline of the tile
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255 ,255);
+  SDL_RenderDrawRect(renderer, tile);
+
+  if (type == WRONG || type == RIGHT_WRONG_POSITION || type == RIGHT_RIGHT_POSITION) {
+    // Render the character on top of the thing
+    SDL_Color text_colour = {CURDLE_TEXT_COLOUR_R, CURDLE_TEXT_COLOUR_G, CURDLE_TEXT_COLOUR_B, 255};
+    SDL_Surface* surface_text = TTF_RenderText_Solid(font, letter, text_colour);
+    SDL_Texture* texture_text = SDL_CreateTextureFromSurface(renderer, surface_text);
+
+    // Adjust the bounds of the letter so it doesn't stretch it out over the entire tile which looks ugly
+
+    tile->x += 2 * CURDLE_WINDOW_SCALE;
+    tile->y -= 2 * CURDLE_WINDOW_SCALE;
+    tile->w -= 4 * CURDLE_WINDOW_SCALE;
+    tile->h += 4 * CURDLE_WINDOW_SCALE;
+
+    SDL_RenderCopy(renderer, texture_text, NULL, tile);
+
+
+    // Free stuff up here?
+    SDL_FreeSurface(surface_text);
+    SDL_DestroyTexture(texture_text);
+  }
 
 }
--- render.h
+++ render.h
@@ -1,6 +1,6 @@
 #ifndef CURDLE_RENDER_H
 #define CURDLE_RENDER_H
-
+#include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL.h>
 
 #define CURDLE_WINDOW_SCALE 4
@@ -16,6 +16,12 @@
 #define CURDLE_SDL_WINDOW_FAILURE 2
 #define CURDLE_SDL_RENDERER_FAILURE 3
 
+enum rectangle_draw_type {
+  BLANK,
+  WRONG,
+  RIGHT_WRONG_POSITION,
+  RIGHT_RIGHT_POSITION
+};
 
 /**
  * Initialises SDL and draws to the window.
@@ -26,6 +32,7 @@ int start_window();
 
 void draw_rect(SDL_Renderer* renderer);
 
-int event_poll(SDL_Window *window, SDL_Renderer *renderer);
+int event_poll(SDL_Window *window, SDL_Renderer *renderer, TTF_Font* font);
 
+void draw_tile(SDL_Rect* tile, SDL_Renderer* renderer, enum rectangle_draw_type type, uint8_t row, uint8_t column, char* letter, TTF_Font* font);
 #endif // Curdle Render Header
