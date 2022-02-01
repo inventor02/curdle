bebac7ec935ecb195ca0ed72e09e652524ea23a6
Adam <adam.caine10@gmail.com>
2022-02-01 11:51:43 +0000

    Added backspace capture

--- game.c
+++ game.c
@@ -103,11 +103,16 @@ struct guess guess_comparator(struct game *game) {
 
 void append_letter(struct game *game, char current_letter){
   printf("append_letter\n");
-  printf("%c\n", current_letter);
-  printf("%s\n", game->current_guess);
+  printf("Current Letter: %c\n", current_letter);
+  printf("Current Guess: %s\n", game->current_guess);
   for(uint8_t i = 0; i < CURDLE_WORD_LENGTH; i++){
+<<<<<<< Updated upstream
     printf("%i\n", i);
     printf("%c\n", game->current_guess[i]); // SEGFAULT
+=======
+    printf("Current Loop: %i\n", i);
+    printf("Current Guess: %c\n", game->current_guess[i]); // SEGFAULT whenever current_guess is accessed
+>>>>>>> Stashed changes
     if(game->current_guess[i] == 0){
       printf("In IF\n");
       game->current_guess[i] = current_letter;
