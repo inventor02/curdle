65ae7d7faddbd8302f12f73c72c53d899d473847
George Peppard <gjp1g21@soton.ac.uk>
2022-02-01 00:17:51 +0000

    (#7) read and write methods

--- statistics.c
+++ statistics.c
@@ -1,3 +1,15 @@
+/*
+statistics.curd file format
+========================================
+
+[average time HH]
+[average time MM]
+[average time SS]
+[guess number total 1],[guess number total 2],etc
+[number of games played]
+[number of games won]
+ */
+
 #include "statistics.h"
 
 #include "game.h"
@@ -7,9 +19,15 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <assert.h>
 
+/**
+ * Prints a log message to STDOUT.
+ *
+ * @param message  the message to display
+ */
 void stats_log(char *message) {
-  printf("[statistics.c] %s\n", message);
+  printf("[curdle:statistics] %s\n", message);
 }
 
 /**
@@ -18,22 +36,81 @@ void stats_log(char *message) {
  * @return whether the operation was successful
  */
 bool create_file_if_not_exists() {
-  stats_log("file checking");
+  stats_log("statistics at:");
+  stats_log(CURDLE_STATISTICS_FILE_PATH);
   if (access(CURDLE_STATISTICS_FILE_PATH, F_OK)) {
     stats_log("file exists, check permissions");
+
     if (access(CURDLE_STATISTICS_FILE_PATH, R_OK|W_OK)) {
       stats_log("FATAL: cannot read/write statistics file");
       return false;
     }
+
+    // TODO check the file is in a valid format
+    stats_log("file exists, all good");
+    return true;
   } else {
     stats_log("file does not exist, try create");
 
     FILE *fp = fopen(CURDLE_STATISTICS_FILE_PATH, "r+");
-    // todo
+
+    if (fp == NULL) {
+      stats_log("FATAL: cannot create file");
+      return false;
+    }
+
+    uint32_t *guess_number_totals = calloc(CURDLE_MAX_GUESSES, sizeof(uint32_t));
+
+    struct average_statistics default = {
+      NULL,
+      *guess_number_totals,
+      0,
+      0,
+    };
+
+    if (fwrite(&default, sizeof(struct average_statistics), 1, fp) != 1) {
+      stats_log("FATAL: wrong number of items written");
+      return false;
+    }
+
+    fclose(fp);
+    fp = NULL;
+
+    stats_log("written a default file");
+    return true;
   }
 }
 
-struct average_statistics get_average_statistics_from_fs();
+/**
+ * Gets the average statistics from the statistics file in the FS.
+ *
+ * @return the average statistics of this user
+ */
+struct average_statistics get_average_statistics_from_fs() {
+  stats_log("trying to open file to get stats");
+
+  FILE *fp = fopen(CURDLE_STATISTICS_FILE_PATH, "r+");
+
+  if (fp == NULL) {
+    stats_log("FATAL: cannot open statistics file");
+    return NULL;
+  }
+
+  struct average_statistics *in = malloc(sizeof(struct average_statistics));
+  assert (in != NULL);
+
+  if (fread(in, sizeof(struct average_statistics), 1, fp) != 1) {
+    stats_log("FATAL: wrong number of items read");
+    return NULL;
+  }
+
+  struct average_statistics stats = *in;
+
+  free(in);
+  in = NULL;
+
+  return stats;
+}
 
 char *encode_bytes(char *bytes) {
   // TODO implement this
